c83d331100db1e309b96d6b6eb6823d3a3d0eaf1
huifukejian <xmw486@gmail.com>
2020-06-07 21:23:27 +0800

    Delete k2p.sh

--- k2p.sh
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
-git clone https://github.com/8688Add/luci-theme-argon-dark-mod.git package/luci-theme-argon-dark-mod
-git clone https://github.com/8688Add/luci-app-passwall.git package/luci-app-passwall
-#git clone https://github.com/destan19/OpenAppFilter.git package/luci-app-oaf
-git clone https://github.com/8688Add/small.git package/small
