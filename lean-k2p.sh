284efb75cc8bb06096c230ebf7fbd9a6fde5ee66
huifukejian <xmw486@gmail.com>
2020-06-11 14:54:46 +0800

    Update lean-k2p.sh

--- lean-k2p.sh
+++ lean-k2p.sh
@@ -3,6 +3,7 @@ git clone https://github.com/8688Add/luci-theme-argon-dark-mod.git package/luci-
 #git clone https://github.com/8688Add/luci-app-passwall.git package/luci-app-passwall
 #git clone https://github.com/destan19/OpenAppFilter.git package/luci-app-oaf
 git clone https://github.com/8688Add/small.git package/small
+git clone https://github.com/pymumu/openwrt-smartdns.git package/smartdns
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.4.1/g' package/base-files/files/bin/config_generate
